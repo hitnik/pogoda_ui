0d98a11c0a77cddce1626667e85037a919757436
hitnik <hitnik@gmail.com>
2020-04-26 20:31:50 +0300

    form button "close"

--- src/components/subscribe/forms/index.js
+++ src/components/subscribe/forms/index.js
@@ -2,24 +2,47 @@ import React, { PureComponent } from "react";
 
 import {Segment, Button, Grid, Form, Transition} from 'semantic-ui-react';
 
-export default class SegmentForms extends PureComponent{
+class ButtonFormClose extends PureComponent {
 
+  render () {
+    return <Form.Button onClick={this.props.onClick}>Закрыть</Form.Button>
+  }
+}
+
+class SubscribeForm extends PureComponent{
+
+  render () {
+    const updateFormVisible = this.props.updateFormVisible
+    const form = (
+      <Form widths="equal">
+        <Form.Group>
+          <Form.Input  label="Title" placeholder="Title" />
+          <Form.Input label="Email" placeholder="Email" />
+        </Form.Group>
+        
+          <div className="rightAlign">
+            <Button.Group>
+              <ButtonFormClose floated="right" onClick={updateFormVisible}/>
+            </Button.Group>
+          </div>
+      
+      </Form>
+  );
+
+  return form
+  }
+}
+
+export default class SegmentForms extends PureComponent{
 
     render () {
 
       const {visible, updateFormVisible} = this.props
-      const sibcribeForm = (
-          <Form widths="equal">
-            <Form.Group>
-              <Form.Input  label="Title" placeholder="Title" />
-              <Form.Input label="Email" placeholder="Email" />
-            </Form.Group>
-          </Form>
-      );
+     
 
       return (
           <Transition.Group animation='drop' duration={750}>
-              {visible && sibcribeForm}
+              {visible && <SubscribeForm updateFormVisible={updateFormVisible}/>}
           </Transition.Group>
       )
     }
