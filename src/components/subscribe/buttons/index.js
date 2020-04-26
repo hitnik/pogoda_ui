a7fc94e7356a4b7d0a2dbc3ecbcd464d2a8f7d83
hitnik <hitnik@gmail.com>
2020-04-26 11:38:26 +0300

    separate unsubscribe button

--- src/components/subscribe/buttons/index.js
+++ src/components/subscribe/buttons/index.js
@@ -1,41 +1,43 @@
 import React, { PureComponent } from "react";
 
-import {Segment, Button, Grid, Transition} from 'semantic-ui-react';
+import { Segment, Button, Grid, Transition } from 'semantic-ui-react';
 
 
 class ButtonSubscribe extends PureComponent {
 
-    render () {
-        return <Button positive onClick={this.props.onClick}>Подписаться на рассылку</Button>;
-    }
+  render() {
+    return <Button positive onClick={this.props.onClick}>Подписаться на рассылку</Button>;
+  }
 }
 
 class ButtonUnsubscribe extends PureComponent {
-
+  render() {
+    return <Button negative onClick={this.props.onClick}>Отменить подписку</Button>
+  }
 }
 
-export default class ButtonsSubScribeContainer extends PureComponent{
+export default class ButtonsSubScribeContainer extends PureComponent {
 
-        render() {
+  render() {
 
-            const {visible, updateButtonsVisible} = this.props
+    const { visible, updateButtonsVisible } = this.props
 
-            const segmentButtons = (
-                <Grid>
+    const segmentButtons = (
+      <Grid>
                   <Grid.Column textAlign="center">
                     <Button.Group fluid>
                       <ButtonSubscribe onClick={updateButtonsVisible}/>
                       <Button.Or />
-                      <Button negative onClick={updateButtonsVisible}>Отменить подписку</Button>
+                      <ButtonUnsubscribe onClick={updateButtonsVisible}/>
                   </Button.Group>
                   </Grid.Column>
-                </Grid> 
-            );
+                </Grid>
+    );
 
-            return (
-                <Transition.Group animation='drop' duration={750}>
+    return (
+      <Transition.Group animation='drop' duration={750}>
                     {visible && segmentButtons}
                 </Transition.Group>
-            )
-        }
+    )
+  }
 }
