c1773bf6a2d6341427fc86b7450b337d5f01812b
hitnik <hitnik@gmail.com>
2020-05-11 22:52:32 +0300

    fetch

--- src/actions/subscribeActions/subscribe.js
+++ src/actions/subscribeActions/subscribe.js
@@ -7,19 +7,33 @@ export default class WeatherAPIConnector {
     constructor () {
         if (!PRODUCTION){
             this._host = WEATHER_API_HOST_DEV;
-            this._schema = this.host + WEATHER_API_SCHEMA_DEV;
+            this._schema = this._host + WEATHER_API_SCHEMA_DEV;
         }
         else {
             this._host = WEATHER_API_PROD;
-            this._schema = this.host+ WEATHER_API_SCHEMA_PROD;   
+            this._schema = this._host+ WEATHER_API_SCHEMA_PROD;   
         }
+        console.log(this._schema)
         const schemaJSON = this._get_schema()
 
     }
 
     async _get_schema () {
-        const responce = await fetch(this._schema);
-        console.log(responce.status);
+       await fetch(this._schema, {
+                method: 'GET',
+                mode: 'no-cors',
+                cache: 'no-cache',
+                headers: {
+                    'Content-Type': 'application/vnd.oai.openapi'
+                  },
+        }
+        ).then(function(response) {
+            return response.text().then(function(text) {
+             console.log(text);
+            })}
+
+        );
+       
     }
 }
 
