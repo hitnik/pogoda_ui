71980a740ae3165754180f2a1647d7d4639d8cf7
hitnik <hitnik@gmail.com>
2020-10-03 20:57:27 +0300

    transition spoiler

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,29 +1,37 @@
 import subscribe from "../subscribe/forms/subscribe";
 import React, { useState, useEffect } from "react";
 import SubscribeContainer from '../subscribe';
-import { Accordion, Icon } from 'semantic-ui-react'
+import { Transition, Link, Icon, Container, Grid, Segment, Button } from 'semantic-ui-react'
 
 const HomePage = (props) => {
 
-    const [activeIndex, setActiveIndex] = useState((index)=>{
-        return index
-    });
-
-    const handleAccordionClick = (e ) => {
-
+    const [buttonsVisible, setButtonsVisible] = useState(false)
+    const handleVisibleLinkClick = () => {
+       setButtonsVisible(!buttonsVisible);
     }
 
     return (
-        <Accordion>
-            <Accordion.Title
-                index = {0}
-                active= {activeIndex === 0}
-                onClick = {handleAccordionClick}
-            >
-                 <Icon name='dropdown' />
-                Dropdown
-            </Accordion.Title>
-        </Accordion>
+        <Container className="no-border-bottom">
+        <Segment.Group>
+          <Segment centered="true">
+            <Grid>
+              <Grid.Column textAlign="center">
+               <a className="spoiler" onClick={handleVisibleLinkClick}>
+                     { buttonsVisible ? <Icon name='chevron down'/> : <Icon name='chevron right'/> }
+                    <span className="dotted"> Оформление/отмена подписки на рассылку штормовых предупреждений </span>
+                </a>
+                <Transition visible={buttonsVisible} animation='slide down' duration={500}>
+                   <span>Text</span>
+                </Transition>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment basic={true}>   
+
+          </Segment>  
+        </Segment.Group>
+      </Container>
+        
     )
 
 }
